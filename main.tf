3d01587735db7fa1b2aaffc1b1015f14c856e290
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-08 17:40:24 +1000

    update module version

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "awscc_datazone_environment_profile" "this" {
   aws_account_id                   = each.value.aws_account_id
   aws_account_region               = each.value.region
   domain_identifier                = var.domain_id
-  environment_blueprint_identifier = each.value.environment_blueprint_identifier
+  environment_blueprint_identifier = coalesce(var.blueprint_map[each.value.environment_blueprint_identifier].environment_blueprint_id, each.value.environment_blueprint_identifier )
   name                             = each.value.name
   description                      = try(each.value.description)
   project_identifier               = coalesce(var.project_map[each.value.project_identifier].project_id, each.value.project_identifier )
