6a8123174d496b6b6a96cd94f8240e3661072111
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-08 19:40:29 +1000

    add name to outputs

--- examples/default_data_lake_example/main.tf
+++ examples/default_data_lake_example/main.tf
@@ -6,7 +6,7 @@ data "tfe_outputs" "domain" {
 
 module "datazone_environment" {
   source  = "app.terraform.io/tfc-demo-au/datazone-environments/awscc"
-  version = "~>  0.1.4"
+  version = "~>  0.1.5"
 
   region                        = var.region
   domain_id                     = coalesce(var.domain_id, data.tfe_outputs.domain.values.datazone_domain_id)
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "datazone_environment_profile" {
   value = [for k, v in awscc_datazone_environment_profile.this : {
     id                       = v.id
+    name                     = v.name
     domain_id                = v.domain_id
     environment_blueprint_id = v.environment_blueprint_id
     environment_profile_id   = v.environment_profile_id
@@ -12,6 +13,7 @@ output "datazone_environment_profile" {
 output "datazone_environment" {
   value = [for k, v in awscc_datazone_environment.this : {
     id                       = v.id
+    name                     = v.name
     domain_id                = v.domain_id
     environment_blueprint_id = v.environment_blueprint_id
     environment_id           = v.environment_id
