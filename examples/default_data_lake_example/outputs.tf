47ff23a86773854e2a67c9290ec8654b387435f3
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-08 17:49:17 +1000

    add outputs

---
+++ examples/default_data_lake_example/outputs.tf
@@ -0,0 +1,7 @@
+output "environment_profile_identifier" {
+  value = module.datazone_environment.datazone_environment_profile
+}
+
+output "awscc_datazone_environment" {
+  value = module.datazone_environment.datazone_environment
+}
---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "datazone_environment_profile" {
+  value = awscc_datazone_environment_profile.this
+  description = "datazone environment profile"
+}
+
+output "datazone_environment" {
+  value = awscc_datazone_environment.this
+  description = "datazone environment"
+}
